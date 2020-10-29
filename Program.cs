6a05e5ab3a286c89e2b34df9887a8b3abaca7ae1
Evgenii86 <73522392+Evgenii86@users.noreply.github.com>
2020-10-29 20:23:15 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ITMO.TechProg.Test
+namespace Training.Example.TestConsole
 {
     class Program
     {
-        static int search(int[] array, int n, int key)
+        static bool PresenceOfWater(int water)
         {
-
-            for (int index = 0; index < n; index ++)
-                if (array[index] == key)
-                    return index;
-            return -1;
-
+            return water != 0;
+        }
+        static bool ConditionCheck(int i, int t)
+        {
+            return i <= t;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Программа производит поиск элементов в заданном массиве");
-            int[] array = { 48, 85, 17, 35, 63, 11, 72 };
-            int n = array.Length;
-            Console.WriteLine("Введите значение искомого ключа");
-            int key = int.Parse(Console.ReadLine());
-            int res = (search(array, n, key));
-            if (res != -1)
-                Console.WriteLine("Искомый ключ найден в позиции " + (res + 1));
-            else
-                Console.WriteLine("Искомый ключ не найден");
-            Console.WriteLine("Нажмите любую клавишу для завершения программы");
-            Console.ReadKey();
+            //Начало
+            
+            //Ввод данных
 
+            Console.WriteLine("Если вода есть, введите 1. Если воды нет, введите 0");
+            int water = int.Parse(Console.ReadLine());
+
+            //Условие
+            if (PresenceOfWater(water))
+            {
+                Console.WriteLine("Введите количество тарелок, которые необходимо вымыть.");
+               
+                int t = int.Parse(Console.ReadLine());
+                int i = 1;
+                while (ConditionCheck(i, t))
+                {
+                    //Действия
+                    Console.WriteLine("Включить воду");
+                    Console.WriteLine("Взять губку");
+                    Console.WriteLine("Нанести моющее средство");
+                    Console.WriteLine("Протереть тарелку губкой");
+                    Console.WriteLine("Смыть моющее средство");
+                    i = i + 1;
+                }
+                    //Вывод данных
+                Console.WriteLine("Итог: количество вымытых тарелок = " + t.ToString());
+            }
+        
+            Console.ReadKey();
         }
     }
 }

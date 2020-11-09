a09d021697ed7b89997bad5341b6a87e7b7e8798
Evgenii86 <73522392+Evgenii86@users.noreply.github.com>
2020-11-09 07:00:10 +0300

    Add files via upload

---
+++ Divider/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divider
+{
+    class Dividelt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Пожалуйста введите первое число");
+                string temp = Console.ReadLine();
+                int i = int.Parse(temp);
+                Console.WriteLine("Пожалуйста введите второе число");
+                temp = Console.ReadLine();
+                int j = int.Parse(temp);
+                int k = i / j;
+                Console.WriteLine("Результат деления равен: " + k);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Возникло исключение: {0}", e);
+            }
+        }
+    }
+}

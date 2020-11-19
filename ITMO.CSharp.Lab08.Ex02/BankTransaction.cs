91846416d509179be83b6774a69356172ecb4037
Evgenii86 <73522392+Evgenii86@users.noreply.github.com>
2020-11-19 10:08:15 +0300

    Add files via upload

---
+++ ITMO.CSharp.Lab08.Ex02/BankTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+    public decimal Amount()
+    {
+        return amount;
+    }
+    public DateTime When()
+    {
+        return when;
+    }
+    public BankTransaction(decimal tranAmount)
+    {
+        when = DateTime.Now;
+        amount = tranAmount;
+    }
+}
